48cc0caf1c332cc84fab405c03bb61af976949b5
Ssempogo Ezra <pogoezra@gamil.com>
2024-08-13 13:25:20 +0300

    Resolution button and functionality set.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const ArtBoard = document.getElementById('ArtBoard');
 const pixels = document.getElementsByClassName('pixel');
-const ResolutionBtn = document.getElementById('Resolution');  
+const ResolutionBtn = document.getElementById('ResolutionBtn');  
 
 
-function PixelGrid(Num = 16){
+function PixelGrid(Num=16){
     for(let i=1;i<(Num*Num);i++){
         const div = document.createElement('div');
         div.style.width=`calc(100%/${Num})`;
@@ -11,13 +11,36 @@ function PixelGrid(Num = 16){
         div.classList.add('pixel');
         ArtBoard.appendChild(div);
     };
+    for(let i = 0; i < pixels.length; i++){
+        pixels[i].addEventListener('mouseover',() => {
+            pixels[i].style.backgroundColor = 'red';
+        });
+    }
 };
 PixelGrid();
 
+ResolutionBtn.addEventListener('click',() =>{
+    let userInput;
+    while(true){
+    userInput= prompt('Enter Resolution desired,must be below 100.');
+    userInput = parseInt(userInput,10);
+    //check if number is in the range 0 to 100
+        if(!isNaN(userInput) && userInput > 0 && userInput <= 100){
+            break;
+        }else{
+            alert('Invalid input');
+        }
+    }
 
-for(let i = 0; i < pixels.length; i++){
-    pixels[i].addEventListener('mouseover',() => {
-        pixels[i].style.backgroundColor = 'red';
-    });
-}
+    const PixelDensity = userInput;
+    ArtBoard.innerHTML="";
+    PixelGrid(PixelDensity);
+});
+
+
+// for(let i = 0; i < pixels.length; i++){
+//     pixels[i].addEventListener('mouseover',() => {
+//         pixels[i].style.backgroundColor = 'red';
+//     });
+// }
 
